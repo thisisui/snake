3576449568615d866da0c6b5fc2ed2ca71c80947
Igor Napierala <igor.napierala@cognifide.com>
2021-01-07 12:03:50 +0100

    basic view

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const gameScreen = document.getElementById("gameScreen");
 
 let canvas, ctx;
 
+const gameState = {
+  player: {
+    pos: {
+      x: 3,
+      y: 10,
+    },
+    vel: {
+      x: 1,
+      y: 0,
+    },
+    snake: [
+      { x: 1, y: 10 },
+      { x: 2, y: 10 },
+      { x: 3, y: 10 },
+    ],
+  },
+  food: {
+    x: 7,
+    y: 7,
+  },
+  gridsize: 20,
+};
+
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
@@ -23,3 +46,28 @@ function keydown(e) {
 }
 
 init();
+
+function paintGame(state) {
+  ctx.fillStyle = BG_COLOUR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const { food, gridsize } = state;
+  const size = canvas.width / gridsize; //600px/20squares = 30px per square
+
+  ctx.fillStyle = FOOD_COLOUR;
+  ctx.fillRect(food.x * size, food.y * size, size, size);
+
+  paintPlayer(state.player, size, SNAKE_COLOUR);
+}
+
+function paintPlayer(playerState, size, colour) {
+  const { snake } = playerState;
+
+  ctx.fillStyle = colour;
+
+  for (let cell of snake) {
+    ctx.fillRect(cell.x * size, cell.y * size, size, size);
+  }
+}
+
+paintGame(gameState);
