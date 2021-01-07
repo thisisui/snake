cdfd67ca81fa9d779655b4ceaf3e8afaf6b3b6bb
Igor Napierala <igor.napierala@cognifide.com>
2021-01-07 12:28:26 +0100

    socket io connected

--- public/index.js
+++ public/index.js
@@ -2,6 +2,10 @@ const BG_COLOUR = "#231f20";
 const SNAKE_COLOUR = "#c2c2c2";
 const FOOD_COLOUR = "#e66916";
 
+const socket = io("http://127.0.0.1:3000", { transport: ["websocket"] });
+
+socket.on("init", handleInit);
+
 const gameScreen = document.getElementById("gameScreen");
 
 let canvas, ctx;
@@ -71,3 +75,7 @@ function paintPlayer(playerState, size, colour) {
 }
 
 paintGame(gameState);
+
+function handleInit(msg) {
+  console.log(msg);
+}
