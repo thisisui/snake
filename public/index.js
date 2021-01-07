610e9bb95a6e3cb871143d5d9c08c5acf514d841
Igor Napierala <igor.napierala@cognifide.com>
2021-01-07 17:02:17 +0100

    snake with room bug

--- public/index.js
+++ public/index.js
@@ -7,12 +7,39 @@ const socket = io("http://127.0.0.1:3000");
 socket.on("init", handleInit);
 socket.on("gameState", handleGameState);
 socket.on("gameOver", handleGameOver);
+socket.on("gameCode", handleGameCode);
+socket.on("unknownGame", handleUnknownGame);
+socket.on("tooManyPlayers", handleTooManyPlayers);
 
 const gameScreen = document.getElementById("gameScreen");
+const initialScreen = document.getElementById("initialScreen");
+const newGameButton = document.getElementById("newGameButton");
+const joinGameButton = document.getElementById("joinGameButton");
+const gameCodeInput = document.getElementById("gameCodeInput");
+const gameCodeDisplay = document.getElementById("gameCodeDisplay");
+
+newGameButton.addEventListener("click", newGame);
+joinGameButton.addEventListener("click", joinGame);
+
+function newGame() {
+  socket.emit("newGame");
+  init();
+}
+
+function joinGame() {
+  const code = gameCodeInput.value;
+  socket.emit("joinGame", code);
+  init();
+}
 
 let canvas, ctx;
+let playerNumber;
+let gameActive = false;
 
 function init() {
+  initialScreen.style.display = "none";
+  gameScreen.style.display = "block";
+
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
 
@@ -22,13 +49,12 @@ function init() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   document.addEventListener("keydown", keydown);
+  gameActive = true;
 }
 
 function keydown(e) {
   socket.emit("keydown", e.keyCode);
 }
-
-init();
 
 function paintGame(state) {
   ctx.fillStyle = BG_COLOUR;
@@ -40,7 +66,8 @@ function paintGame(state) {
   ctx.fillStyle = FOOD_COLOUR;
   ctx.fillRect(food.x * size, food.y * size, size, size);
 
-  paintPlayer(state.player, size, SNAKE_COLOUR);
+  paintPlayer(state.players[0], size, SNAKE_COLOUR);
+  paintPlayer(state.players[1], size, "blue");
 }
 
 function paintPlayer(playerState, size, colour) {
@@ -53,15 +80,51 @@ function paintPlayer(playerState, size, colour) {
   }
 }
 
-function handleInit(msg) {
-  console.log(msg);
+function handleInit(number) {
+  playerNumber = number;
 }
 
 function handleGameState(gameState) {
+  if (!gameActive) {
+    return;
+  }
   gameState = JSON.parse(gameState);
   requestAnimationFrame(() => paintGame(gameState));
 }
 
-function handleGameOver() {
-  alert("End of the game! you lose!");
+function handleGameOver(data) {
+  if (!gameActive) {
+    return;
+  }
+  data = JSON.parse(data);
+
+  if (data.winner === playerNumber) {
+    alert("You win!");
+  } else {
+    alert("You lose!");
+  }
+
+  gameActive = false;
+}
+
+function handleGameCode(gameCode) {
+  gameCodeDisplay.innerText = gameCode;
+}
+
+function handleUnknownGame() {
+  reset();
+  alert("Unknown game code");
+}
+
+function handleTooManyPlayers() {
+  reset();
+  alert("This game is already in progress");
+}
+
+function reset() {
+  playerNumber = null;
+  gameCodeInput.value = "";
+  gameCodeDisplay.innerText = "";
+  initialScreen.style.display = "block";
+  gameScreen.style.display = "none";
 }
--- server/game.js
+++ server/game.js
@@ -1,32 +1,52 @@
 const { GRID_SIZE } = require("./constants");
 
 module.exports = {
-  createGameState,
+  initGame,
   gameLoop,
   getUpdatedVelocity,
 };
 
+function initGame() {
+  const state = createGameState();
+  randomFood(state);
+  return state;
+}
+
 function createGameState() {
   return {
-    player: {
-      pos: {
-        x: 3,
-        y: 10,
+    players: [
+      {
+        pos: {
+          x: 3,
+          y: 10,
+        },
+        vel: {
+          x: 1,
+          y: 0,
+        },
+        snake: [
+          { x: 1, y: 10 },
+          { x: 2, y: 10 },
+          { x: 3, y: 10 },
+        ],
       },
-      vel: {
-        x: 1,
-        y: 0,
+      {
+        pos: {
+          x: 18,
+          y: 10,
+        },
+        vel: {
+          x: 1,
+          y: 0,
+        },
+        snake: [
+          { x: 20, y: 10 },
+          { x: 19, y: 10 },
+          { x: 18, y: 10 },
+        ],
       },
-      snake: [
-        { x: 1, y: 10 },
-        { x: 2, y: 10 },
-        { x: 3, y: 10 },
-      ],
-    },
-    food: {
-      x: 7,
-      y: 7,
-    },
+    ],
+    food: {},
     gridsize: GRID_SIZE,
   };
 }
@@ -36,10 +56,14 @@ function gameLoop(state) {
     return;
   }
 
-  const playerOne = state.player;
+  const playerOne = state.players[0];
+  const playerTwo = state.players[1];
 
   playerOne.pos.x += playerOne.vel.x;
   playerOne.pos.y += playerOne.vel.y;
+
+  playerTwo.pos.x += playerTwo.vel.x;
+  playerTwo.pos.y += playerTwo.vel.y;
 
   if (
     playerOne.pos.x < 0 ||
@@ -50,10 +74,26 @@ function gameLoop(state) {
     return 2;
   }
 
+  if (
+    playerTwo.pos.x < 0 ||
+    playerTwo.pos.x > GRID_SIZE ||
+    playerTwo.pos.y < 0 ||
+    playerTwo.pos.y > GRID_SIZE
+  ) {
+    return 1;
+  }
+
   if (state.food.x === playerOne.pos.x && state.food.y === playerOne.pos.y) {
     playerOne.snake.push({ ...playerOne.pos });
     playerOne.pos.x += playerOne.vel.x;
     playerOne.pos.y += playerOne.vel.y;
+    randomFood(state);
+  }
+
+  if (state.food.x === playerTwo.pos.x && state.food.y === playerTwo.pos.y) {
+    playerTwo.snake.push({ ...playerTwo.pos });
+    playerTwo.pos.x += playerTwo.vel.x;
+    playerTwo.pos.y += playerTwo.vel.y;
     randomFood(state);
   }
 
@@ -68,6 +108,17 @@ function gameLoop(state) {
     playerOne.snake.shift();
   }
 
+  if (playerTwo.vel.x || playerTwo.vel.y) {
+    for (let cell of playerTwo.snake) {
+      if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
+        return 1;
+      }
+    }
+
+    playerTwo.snake.push({ ...playerTwo.pos });
+    playerTwo.snake.shift();
+  }
+
   return false;
 }
 
@@ -77,7 +128,13 @@ function randomFood(state) {
     y: Math.floor(Math.random() * GRID_SIZE),
   };
 
-  for (let cell of state.player.snake) {
+  for (let cell of state.players[0].snake) {
+    if (cell.x === food.x && cell.y === food.y) {
+      return randomFood(state);
+    }
+  }
+
+  for (let cell of state.players[1].snake) {
     if (cell.x === food.x && cell.y === food.y) {
       return randomFood(state);
     }
